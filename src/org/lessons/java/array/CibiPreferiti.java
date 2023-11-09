06ad33601a7c5f7662a5d0e05b66120d95e29d7a
Flopiras <Flopiras@yahoo.it>
2023-11-09 17:58:41 +0100

    add comments and bonus

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -4,14 +4,22 @@ public class CibiPreferiti {
 
 	public static void main(String[] args) {
 		
+		//array di cibi preferiti
+		String foods[] = {"pizza", "pasta", "minestrone", "patatine", "sushi", "bistecche"};
 		
-		String foods[] = {"pizza", "pasta", "minestrone", "patatine", "sushi"};
-		
+		//stampo la lunghezza della classifica
 		System.out.println("La classifica contiene " + foods.length + " elementi");
 		
+		//stampo il cibo al primo posto nella lista (cibo preferito)
 		System.out.println("Il tuo cibo preferito è: " + foods[0]);
 		
+		//stampo il cibo all'ultimo posto nella lista (cibo preferito ma non troppo)
 		System.out.println("All'ultimo posto della classifica c'è: " + foods[foods.length - 1]);
+		
+		//stampo il cibo/i cibi in mezzo alla classifica
+		int avg = foods.length / 2;
+		
+		System.out.println("Al centro c'è: " + foods[avg]);
 		
 		
 
